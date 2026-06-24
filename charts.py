a8960e50d14a35946de21434fb6c3636d3eb4c62
maciej <wozniak.maciej.a@gmail.com>
2026-06-24 21:35:00 +0200

    Aktualizacja projektu 24/06/2026

--- charts.py
+++ charts.py
@@ -1,48 +1,31 @@
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import plotly.express as px
 
 def acc_group_borough(df):
+    df_temp = df.copy()
+    df_temp['BOROUGH'] = df_temp['BOROUGH'].fillna("NIEZNANA (NaN)")
 
-    dane_wykres = df.groupby("BOROUGH", dropna=False).size()
-    dane_wykres.index = dane_wykres.index.fillna("NIEZNANA (NaN)")
-    dane_wykres = dane_wykres.sort_values(ascending=False)
-    dzielnice = dane_wykres.index.tolist()
-    wartosci = dane_wykres.values.tolist()
+    dane_wykres = df_temp.groupby("BOROUGH").size().reset_index(name="Liczba rekordów")
+    dane_wykres = dane_wykres.sort_values(by="Liczba rekordów", ascending=False)
 
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    kolory = [cm.viridis(i / len(dzielnice)) for i in range(len(dzielnice))]
-
-    bars = ax.bar(dzielnice, wartosci, color=kolory, edgecolor="black", linewidth=0.7)
-
-    ax.set_title(
-        "Liczba kolizji w podziale na dzielnice NYC", fontsize=14, pad=15
-    )
-    ax.set_xlabel("Dzielnica (Borough)", fontsize=12, labelpad=10)
-    ax.set_ylabel("Liczba rekordów", fontsize=12, labelpad=10)
-
-    ax.set_xticks(range(len(dzielnice)))
-    ax.set_xticklabels(dzielnice, rotation=45, ha="right")
-    ax.get_yaxis().set_major_formatter(
-        ticker.FuncFormatter(lambda x, p: format(int(x), ",d").replace(",", " "))
+    fig = px.bar(
+        dane_wykres,
+        x="BOROUGH",
+        y="Liczba rekordów",
+        text="Liczba rekordów",
+        color="Liczba rekordów",
+        color_continuous_scale="Viridis",
+        title="Liczba kolizji w podziale na dzielnice NYC",
+        labels={"BOROUGH": "Dzielnica (Borough)", "Liczba rekordów": "Liczba rekordów"}
     )
 
-    ax.grid(axis="y", linestyle="--", alpha=0.7, zorder=0)
-    ax.set_axisbelow(True)
+    fig.update_layout(
+        title_font_size=16,
+        title_x=0.5,
+        xaxis_tickangle=45,
+        coloraxis_showscale=False,
+        template="plotly_white"
+    )
 
-    for bar in bars:
-        yval = bar.get_height()
-        sformatowana_liczba = format(int(yval), ",d").replace(",", " ")
-        ax.text(
-            bar.get_x() + bar.get_width() / 2,
-            yval + (max(wartosci) * 0.01),
-            sformatowana_liczba,
-            ha="center",
-            va="bottom",
-            fontsize=10,
-        )
+    fig.update_traces(textposition="outside")
 
-    plt.tight_layout()
-
-    plt.show()
+    fig.show()
--- map.py
+++ map.py
@@ -1,15 +1,32 @@
 import plotly.express as px
 
-def accident_map(df_filtered):
+def accident_map(df):
+    df_temp = df.copy()
 
-    fig = px.scatter_map(
+    df_temp['ON STREET NAME'] = df_temp['ON STREET NAME'].astype(str).str.strip().str.upper()
+
+    niechciane_ulice = ['', 'NAN', 'UNSPECIFIED', 'UNKNOWN', 'UnKnOwN', 'OFF STREET', 'OFF-STREET']
+    czyste_ulice = df_temp[~df_temp['ON STREET NAME'].isin(niechciane_ulice)]
+
+    top5_nazwy_ulic = czyste_ulice['ON STREET NAME'].value_counts().head(5).index
+
+    df_filtered = czyste_ulice[
+        (czyste_ulice['ON STREET NAME'].isin(top5_nazwy_ulic)) &
+        (czyste_ulice['LATITUDE'].notna()) &
+        (czyste_ulice['LONGITUDE'].notna()) &
+        (czyste_ulice['LATITUDE'] != 0)
+        ]
+
+    df_filtered = df_filtered.sort_values(by=['ON STREET NAME', 'LONGITUDE'])
+
+    fig = px.line_map(
         df_filtered,
         lat='LATITUDE',
         lon='LONGITUDE',
-        zoom=10,
-        center={"lat": 40.7128, "lon": -74.0060},
-        hover_name='BOROUGH',
+        color='ON STREET NAME',
+        hover_name='ON STREET NAME',
         hover_data={
+            'BOROUGH': True,
             'ACCIDENT DATE': True,
             'ACCIDENT TIME': True,
             'CONTRIBUTING FACTOR VEHICLE 1': True,
@@ -17,12 +34,21 @@ def accident_map(df_filtered):
             'LATITUDE': False,
             'LONGITUDE': False
         },
+        zoom=10,
+        center={"lat": 40.7128, "lon": -74.0060},
         map_style="carto-positron",
         height=750,
-        title="Mapa wypadków w Nowym Jorku"
+        title="Mapa "
     )
 
-    fig.update_traces(cluster=dict(enabled=True, color="yellow", size=10))
-    fig.update_layout(margin={"r": 0, "t": 40, "l": 0, "b": 0})
+    fig.update_traces(line=dict(width=3))
+
+    fig.update_layout(
+        margin={"r": 0, "t": 40, "l": 0, "b": 0},
+        title_text="Geometria TOP 5 najniebezpieczniejszych ulic w NYC",
+        title_x=0.5,
+        legend_title_text="Nazwa ulicy",
+        template="plotly_white"
+    )
 
     return fig
